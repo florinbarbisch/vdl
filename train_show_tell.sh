0bba2234b13c798a0f4b8f8d11829f0fef5cb8b3
florinbarbisch <40719099+florinbarbisch@users.noreply.github.com>
2024-12-26 17:00:39 +0100

    Added: slurm scripts

---
+++ train_show_tell.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=4  # matches num_workers in training script
+#SBATCH --gres=gpu:1  # requires 1 GPU
+#SBATCH --partition=performance
+#SBATCH --output=logs/show_tell_%A_%a.out
+#SBATCH --error=logs/show_tell_%A_%a.err
+#SBATCH --mem=16G
+
+# Create necessary directories
+mkdir -p logs
+mkdir -p checkpoints
+
+# Run training script
+.venv/bin/python src/train.py --model show_tell --debug overfit
