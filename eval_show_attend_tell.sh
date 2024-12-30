bc61d676a823966df48d223c180f38bd2b3e6bdf
florinbarbisch <40719099+florinbarbisch@users.noreply.github.com>
2024-12-30 17:29:05 +0100

    Added: sbatch eval scripts

---
+++ eval_show_attend_tell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=4  # matches num_workers in training script
+#SBATCH --gres=gpu:1  # requires 1 GPU
+#SBATCH --partition=performance
+#SBATCH --output=logs/eval_show_attend_tell_%A_%a.out
+#SBATCH --error=logs/eval_show_attend_tell_%A_%a.err
+#SBATCH --mem=16G
+
+
+# Run evaluation script
+.venv/bin/python eval.py --model show_attend_tell --checkpoint_path $1
